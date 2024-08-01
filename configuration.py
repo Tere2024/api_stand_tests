aeed53d9ab85bceca9645c90f539207122d1a63b
tere <tere@@LAPTOP-J9B0LHE8>
2024-07-31 22:05:46 -0400

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-b146a62d-fc60-47ca-b92a-74cb0fe91068.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
